6050730731c3d9a65b0cabe7a63afda59344ca25
césar silva <cesar.silva@wiremaze.com>
2014-06-08 14:44:17 +0100

    Add a full_title helper

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home Page" do
+
+#    it "should exist" do
+#      visit '/static_pages/home'
+#      response.status.should be(200)
+#    end
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+  end
+
+
+  describe "Help Page" do
+
+#    it "should exist" do
+#      visit '/static_pages/home'
+#      response.status.should be(200)
+#    end
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Sample App')
+    end
+
+  end
+
+
+  describe "About Page" do
+
+#    it "should exist" do
+#      visit '/static_pages/home'
+#      response.status.should be(200)
+#    end
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('Sample App')
+    end
+
+  end
+
+
+end
+
