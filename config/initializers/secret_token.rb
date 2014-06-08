879bd4462283bf26407898315d0af025ed419c01
césar silva <cesar.silva@wiremaze.com>
2014-06-08 11:10:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FnkpApp::Application.config.secret_key_base = '2cb33162dbd80a27fd3f64e9955f791489c0f959ca9d7a424cd8c5f0a68ad2373cf35b48b0845d62f0cbb783dca0f15f8692eb4156be8ddce98fd6e28642ce83'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FnkpApp::Application.config.session_store :cookie_store, key: '_fnkp_app_session'
